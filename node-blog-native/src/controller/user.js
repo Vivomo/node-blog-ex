b103744387ec54932e568d5494a121ea7b43be02
Vimo <929992114@qq.com>
2020-01-19 03:36:03 +0800

    user login router

---
+++ node-blog-native/src/controller/user.js
@@ -0,0 +1,8 @@
+const login = (username, password) => {
+    console.log('login', username, password);
+    return true;
+};
+
+module.exports = {
+    login,
+};
--- node-blog-native/src/router/user.js
+++ node-blog-native/src/router/user.js
@@ -1,21 +1,29 @@
+const {SuccessModel, ErrorModel} = require('../model/resModel');
+const {login} = require('../controller/user');
+
 const handleRouter = (req, res) => {
     const {method, path} = req;
 
     const router = {
         POST: {
-            '/api/user/login': () => 'login',
+            '/api/user/login': (req) => {
+                let {username, password} = req.body;
+                let result = login(username, password);
+                if (result) {
+                    return new SuccessModel();
+                }
+                return new ErrorModel('login false');
+            },
         }
     };
 
-    let msg;
+    let data;
     try {
-        msg = router[method][path]();
+        data = router[method][path](req, res);
     } catch (e) {
-        return;
+
     }
-    return {
-        msg
-    };
+    return data;
 };
 
 module.exports = handleRouter;
